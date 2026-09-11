12046142bc0a2691e2d72a0aced7093427b069c3
arzafran <franco@darkroom.engineering>
2026-09-11 02:27:12 +0200

    fix: create hook state directory before acquiring its lock

--- CLI/CLI+Hooks.swift
+++ CLI/CLI+Hooks.swift
@@ -155,6 +155,8 @@ final class ClaudeHookSessionStore {
     }
 
     private func withLockedState<T>(_ body: (inout ClaudeHookSessionStoreFile) throws -> T) throws -> T {
+        let parentURL = URL(fileURLWithPath: statePath).deletingLastPathComponent()
+        try fileManager.createDirectory(at: parentURL, withIntermediateDirectories: true, attributes: nil)
         let lockPath = statePath + ".lock"
         let fd = open(lockPath, O_CREAT | O_RDWR, mode_t(S_IRUSR | S_IWUSR))
         if fd < 0 {
@@ -187,8 +189,6 @@ final class ClaudeHookSessionStore {
 
     private func saveUnlocked(_ state: ClaudeHookSessionStoreFile) throws {
         let stateURL = URL(fileURLWithPath: statePath)
-        let parentURL = stateURL.deletingLastPathComponent()
-        try fileManager.createDirectory(at: parentURL, withIntermediateDirectories: true, attributes: nil)
         let data = try encoder.encode(state)
         try data.write(to: stateURL, options: .atomic)
     }
